2e3e8b09d4cc83667d41b955325976bc6b85e5f8
Emmanuel Holonou <holonouemmanuel0@gmail.com>
2022-09-22 09:56:33 +0100

    Reduce the size of the images

--- src/containers/blog/index.js
+++ src/containers/blog/index.js
@@ -1,5 +1,5 @@
-export {default as blog1} from '../../assets/blog01.png';
+export {default as blog1} from '../../assets/blog01.webp';
 export {default as blog2} from '../../assets/blog02.png';
-export {default as blog3} from '../../assets/blog03.png';
-export {default as blog4} from '../../assets/blog04.png';
+export {default as blog3} from '../../assets/blog03.webp';
+export {default as blog4} from '../../assets/blog04.webp';
 export {default as blog5} from '../../assets/blog05.png';
